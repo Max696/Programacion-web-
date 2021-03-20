1dd38a5d4af2e70d7681f7c93e6dd1d7d15e111f
Max696 <30308760+Max696@users.noreply.github.com>
2021-03-19 18:16:47 -0600

    ,

---
+++ New folder (2)/ToDo.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class ToDo extends Component {
+  render() {
+    return (
+      <div>
+       
+        <p>Aqui falta agregar Listas </p>
+        
+      </div>
+    );
+  }
+}
+ 
+export default ToDo;
